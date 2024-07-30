225d17f093dd507b32a16233d2ecc1c5c7d5014b
Your Name <you@example.com>
2024-07-30 15:02:02 +0300

    create reactive forms place-details

--- wheather-app/src/app/components/place-detail/place-detail.component.ts
+++ wheather-app/src/app/components/place-detail/place-detail.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Validators } from '@angular/forms';
 @Component({
   selector: 'app-place-detail',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './place-detail.component.html',
   styleUrl: './place-detail.component.scss'
 })
-export class PlaceDetailComponent {
-createPlace() {
-throw new Error('Method not implemented.');
-}
+export class PlaceDetailComponent implements OnInit {
+  placeForm: FormGroup<{
+    name: FormControl<string | null>;
+    type: FormControl<string | null>;
+    address: FormControl<string | null>;
+  }>;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.placeForm = this.formBuilder.group({
+      name: ['',Validators.required],
+      type: ['',Validators.required],
+      address: ['',Validators.required]
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+  createPlace() {
+    console.log(this.placeForm.value)
+  }
 }
