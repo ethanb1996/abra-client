e994d3b60ebb551951a81d6fd1eac554c188b045
Your Name <you@example.com>
2024-07-30 14:39:05 +0300

    define the structure of the ui

--- wheather-app/src/app/app.component.ts
+++ wheather-app/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PlaceDetailComponent } from "./components/place-detail/place-detail.component";
+import { MapComponent } from "./components/map/map.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PlaceDetailComponent, MapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- wheather-app/src/app/components/place-detail/place-detail.component.ts
+++ wheather-app/src/app/components/place-detail/place-detail.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './place-detail.component.scss'
 })
 export class PlaceDetailComponent {
+createPlace() {
+throw new Error('Method not implemented.');
+}
 
 }
